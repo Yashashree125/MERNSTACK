9c5850bcda7bdc3c64b71eea80cdd483de4a6c0d
Your Name <you@example.com>
2022-01-26 15:25:32 +0530

    MERN

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express ();
+dotenv.config();
+
+
+
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+//const CONNECTION_URL = 'mongodb+srv://mernapp:mernapp123@cluster0.syng0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
+
+
